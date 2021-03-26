fcdf0708ca703294a8fd792112fd61530038b248
etiennezastko <etiennezastko@hotmail.fr>
2021-03-26 10:39:58 +0100

    first commit

---
+++ index.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-eOJMYsd53ii+scO/bJGFsiCZc+5NDVN2yr8+0RDqr0Ql0h+rP48ckxlpbzKgwra6" crossorigin="anonymous"> <!-- Pour faire fonctionner bootstrap-->
+    <link rel="stylesheet" href="style.css"> <!-- Ma feuille de style-->
+</head>
+<body>
+    
+<main class="container">
+    <div class="row">
+        <div class="col-md-8 col-sm-12"></div>
+        <div class="col-md-4 col-sm-12">
+            <canvas id="graphic" width="400" height="400"></canvas>
+        </div>
+    </div>
+</main>
+
+    
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/js/bootstrap.bundle.min.js" integrity="sha384-JEW9xMcG8R+pH31jmWH6WWP0WintQrMb4s7ZOdauHnUtxwoG2vI5DkLtS3qm9Ekf" crossorigin="anonymous"></script> <!-- Pour faire fonctionner bootstrap-->
+    <script src="https://cdn.jsdelivr.net/npm/chart.js@2.9.4/dist/Chart.min.js"></script> <!-- Pour faire fonctionner chart.js-->
+    <script src="main.js"></script> <!-- Mon script-->
+</body>
+</html>
